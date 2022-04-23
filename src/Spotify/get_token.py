0ff135a6bb95673f21477b4df56868e874da35d6
formegusto <formegusto@gmail.com>
2022-04-23 12:54:03 +0900

    update get_token module init

---
+++ src/Spotify/get_token.py
@@ -0,0 +1,29 @@
+import json
+import requests as req
+import base64
+
+
+def get_token():
+    # Step 1. Setting AuthOption
+    json_file = open("auth.json")
+    auth = json.load(json_file)
+    auth_data = "{}:{}".format(
+        auth['CLIENT_ID'],
+        auth['CLIENT_SECRET']
+    )
+    authorization = "Basic " + \
+        base64.b64encode(auth_data.encode('ascii')).decode("ascii")
+
+    # Step 2. Setting Request Datas
+    token_url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": authorization
+    }
+    data = {
+        "grant_type": "client_credentials"
+    }
+
+    # Step 3. Request
+    res = req.post(token_url, data=data, headers=headers)
+
+    return res.json()
